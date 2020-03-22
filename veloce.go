805cc36510a073e13f7d2e3f6a50a6b1c9e02655
isurusiri <isuruanatomy@gmail.com>
2020-03-22 11:23:52 +0530

    added cache craetion logic

--- veloce.go
+++ veloce.go
@@ -2,6 +2,7 @@ package veloce
 
 import (
 	"fmt"
+	"runtime"
 	"sync"
 	"time"
 )
@@ -310,11 +311,56 @@ func stopGarbageCollector(c *cache) {
 	c.garbageCollector.stop <- true
 }
 
-func runGarbageCollector(c *cache, ci time.Duration) {
+func runGarbageCollector(c *cache, cleanUpInterval time.Duration) {
 	gc := &garbageCollector{
-		Interval: ci,
+		Interval: cleanUpInterval,
 		stop:     make(chan bool),
 	}
 	c.garbageCollector = gc
 	go gc.Run(c)
+}
+
+func newCache(duration time.Duration, cacheItems map[string]Item) *cache {
+	if duration == 0 {
+		duration = -1
+	}
+	c := &cache{
+		defaultExpiration: duration,
+		items:             cacheItems,
+	}
+	return c
+}
+
+func newCacheWithGarbageCollector(duration time.Duration, cleanUpInterval time.Duration, cacheItems map[string]Item) *Cache {
+	c := newCache(duration, cacheItems)
+
+	// makesure gc goroutine doesn't clean C (Cache) once it is returned.
+	C := &Cache{c}
+
+	if cleanUpInterval > 0 {
+		runGarbageCollector(c, cleanUpInterval)
+		runtime.SetFinalizer(C, stopGarbageCollector)
+	}
+	return C
+}
+
+// New returns a new cache with a given expiration time duration. A garbage collector is
+// initialized with a given clean up inerval.
+// If the expiration duration is less than one the items in the cache never expire, and
+// must be deleted manually.
+// If the cleanup interval is less than one, expired items are not deleted from the cache
+// before calling c.DeleteExpired().
+func New(defaultExpiration time.Duration, cleanUpInterval time.Duration) *Cache {
+	items := make(map[string]Item)
+	return newCacheWithGarbageCollector(defaultExpiration, cleanUpInterval, items)
+}
+
+// NewForm returns a new cache with a given expiration time duration. A garbage collector is
+// initialized with a given clean up inerval.
+// If the expiration duration is less than one the items in the cache never expire, and
+// must be deleted manually.
+// If the cleanup interval is less than one, expired items are not deleted from the cache
+// before calling c.DeleteExpired().
+func NewForm(defaultExpiration time.Duration, cleanUpInterval time.Duration, items map[string]Item) *Cache {
+	return newCacheWithGarbageCollector(defaultExpiration, cleanUpInterval, items)
 }
